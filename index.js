918968525f367bea23ce128c89dd4ff5b3881dd4
Thribs <thribsilva@gmail.com>
2021-09-02 10:27:13 -0300

    fixed numbers formatting

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ console.log("Attempting to send message")
 client.messages
   .create({
     body: "Hello there!",
-    from: 'whatsapp: +14155238886',
-    to: 'whatsapp: +5535997179655'
+    from: 'whatsapp:+14155238886',
+    to: 'whatsapp:+553597179655'
   })
-  .then(message => console.log(`sent message ${message.sid}`));
+  .then(message => console.log(`sent message ${message.sid}`))
+  .catch(err => console.log(`Sorry. An error has occured.
+     Please report to the administrator. ERROR: ${err}`));
